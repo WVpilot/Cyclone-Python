ee21778f00d729ffb94bf645756f9c1755c7eb15
WVpilot <59519152+WVpilot@users.noreply.github.com>
2024-12-05 14:12:42 -0600

    Base code setup for hoop to be finalized

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
 #import sys
 import argparse
+from planner import Planner
 
 class Cyclone:
 	def __init__(self):
 		self.parser = argparse.ArgumentParser(description="This is a Python port for Cyclone for easier access for SHC")
-		self.add_arguments(self)
+		self.add_arguments()
 
 	def add_arguments(self):
-		self.parser.add_argument("-o", help="Defines the path for the output file of an operation")
-		self.parser.add_argument("-v", help="Verbose output")
-		self.parser.add_argument("--plan", help="Create a .gcode file from an input mesh")
-		self.parser.add_argument("input", help="Input .wind file")
+		self.parser.add_argument("-o", "--output", help="Defines the path for the output file of an operation")
+		self.parser.add_argument("-v", "--verbose", help="Verbose output", action="store_true")
+		self.parser.add_argument("--plan", help="Create a .gcode file from an input mesh", action="store_true")
+		self.parser.add_argument("-i", "--input", help="Input .wind file")
 
 	def parse_args(self):
 		self.args = self.parser.parse_args()
@@ -18,13 +19,13 @@ class Cyclone:
 	def run(self):
 		if self.args.verbose:
 			self.verbose = True
-		
+		if self.args.input & self.args.output:
+			planner = Planner(self.args.input, self.args.output)
+			planner.run()
 
 #Handle argument inputs
-
-
 if __name__ == "__main__":
 	cyclone = Cyclone()
 	cyclone.parse_args()
-
+	cyclone.run()
 	pass
--- src/planner.py
+++ src/planner.py
@@ -1,11 +1,67 @@
 import json
+import numpy
 
 class Planner:
 	def __init__(self, input, output):
 		self.inputPath = input
 		self.outputPath = output
+		self.gcode = ''
+
+	def move(self, x = None, y = None, z = None):
+		self.gcode += 'G1 '
+		if (x != None):
+			self.gcode += f'X{x} '
+		if (y != None):
+			self.gcode += f'Y{y} '
+		if (z != None):
+			self.gcode += f'Z{z} '
+		self.gcode += '\n'
 
 	def run(self):
-		with open(input) as f:
+		#Load the input file
+		with open(self.inputPath) as f:
 			data = json.load(f)
-		data
+		
+		feedRate = data['defaultFeedRate']
+
+		self.x = 0
+		self.y = 0
+		self.z = 0
+
+		#Set positioning to relative and the set position to absolute 0
+		self.gcode += f'G91\n'
+		self.gcode += f'G92 X{self.x} Y{self.y} Z{self.z}\n'
+		
+		#Set feed rate
+		self.gcode += f'G1 F{feedRate}'
+
+		#Iterate through each layer set
+		for layer in data['layers']:
+			match layer['windType']:
+				case 'helical':
+					
+
+					if layer['terminal']:
+						break
+					pass
+				case 'hoop':
+					self.headAngle = 90 - numpy.rad2deg(numpy.atan(data['mandrelParameters']['diameter'] / data['towParameters']['width']))
+					#Lock out the wind layer to secure it, either use input setting or default of one rotation
+					self.y += (layer['lockDegrees'] != 'undefined') if layer['lockDegrees'] else 360
+					self.move(y = self.y)
+
+
+
+					if layer['terminal']:
+						print("Terminal layer encountered, ending wind planning")
+						break
+					pass
+		#Write gcode to file, create a new file is current file does not exist
+		try:
+			print("Writing gcode to output file")
+			with open(self.outputPath, "w") as g:
+				g.write(self.gcode)
+		except:
+			print("Warning: ouput file does not exist, creating a new file using output path")
+			with open(self.outputPath, "x") as g:
+				g.write(self.gcode)
